02a8bdcbf71d7a0e3305db24e6a3a1341b1b89b1
Yannick Le Pennec <yannick.lepennec@gmail.com>
2013-11-28 01:24:23 +0900

    Simplified stuff somewhat

--- Kite/Kite/GraphReader.cs
+++ Kite/Kite/GraphReader.cs
@@ -9,7 +9,6 @@ namespace Kite
 		// Probably the most awful and lazy parsing method I've ever written. Oh well.
 		public static Network BuildNetwork(Stream stream)
 		{
-			var links = new List<string>();
 			var network = new Network();
 
 			using (var reader = new StreamReader(stream))
@@ -18,25 +17,25 @@ namespace Kite
 				while ((line = reader.ReadLine()) != null)
 				{
 					// We only care about links, really.
-					if (line.Contains("--"))
+					var nodeNames = line.Split(new string[] { " -- " }, StringSplitOptions.None);
+
+					if (nodeNames.Length == 2)
 					{
-						links.Add(line.Trim());
+						// Get the nodes or build new ones.
+						var node0 = network.FindNode(nodeNames[0]) ?? new Node(nodeNames[0]);
+						var node1 = network.FindNode(nodeNames[1]) ?? new Node(nodeNames[1]);
+
+						// Link them together
+						// TODO
+
+						// Store them back in the network.
+						network.AddNode(node0);
+						network.AddNode(node1);
 					}
 				}
 			}
-
-			links.ForEach(link => {
-				var nodeNames = link.Split(new string[] { " -- " }, StringSplitOptions.None);
-
-				var node0 = network.FindNode(nodeNames[0]) ?? new Node(nodeNames[0]);
-				var node1 = network.FindNode(nodeNames[1]) ?? new Node(nodeNames[1]);
-
-				network.AddNode(node0);
-				network.AddNode(node1);
-			});
 
 			return network;
 		}
 	}
 }
-
