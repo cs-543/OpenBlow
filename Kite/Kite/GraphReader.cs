2fb5183904bd2ae1f7455f312a31c3705b814d98
Yannick Le Pennec <yannick.lepennec@gmail.com>
2013-11-28 14:27:56 +0900

    Murdered tabs

--- Kite/Kite/GraphReader.cs
+++ Kite/Kite/GraphReader.cs
@@ -6,37 +6,37 @@ namespace Kite
 {
 	public class GraphReader
 	{
-		// Probably the most awful and lazy parsing method I've ever written. Oh well.
-		public static Network BuildNetwork(Stream stream)
-		{
-			var network = new Network();
+    // Probably the most awful and lazy parsing method I've ever written. Oh well.
+    public static Network BuildNetwork(Stream stream)
+    {
+    	var network = new Network();
 
-			using (var reader = new StreamReader(stream))
-			{
-				string line = null;
-				while ((line = reader.ReadLine()) != null)
-				{
-					// We only care about links, really.
-					var switchNames = line.Trim().Split(new string[] { " -- " }, StringSplitOptions.RemoveEmptyEntries);
+    	using (var reader = new StreamReader(stream))
+    	{
+        string line = null;
+        while ((line = reader.ReadLine()) != null)
+        {
+        	// We only care about links, really.
+        	var switchNames = line.Trim().Split(new string[] { " -- " }, StringSplitOptions.RemoveEmptyEntries);
 
-					if (switchNames.Length == 2)
-					{
-						// Get the switches or build new ones.
-						var sw0 = network.FindSwitch(switchNames[0]) ?? new Switch(switchNames[0]);
-						var sw1 = network.FindSwitch(switchNames[1]) ?? new Switch(switchNames[1]);
+        	if (switchNames.Length == 2)
+        	{
+            // Get the switches or build new ones.
+            var sw0 = network.FindSwitch(switchNames[0]) ?? new Switch(switchNames[0]);
+            var sw1 = network.FindSwitch(switchNames[1]) ?? new Switch(switchNames[1]);
 
-						// Link them together
+            // Link them together
                         sw0.AddNeighboor(sw1);
                         sw1.AddNeighboor(sw0);
 
-						// Store them back in the network.
-						network.AddSwitch(sw0);
-						network.AddSwitch(sw1);
-					}
-				}
-			}
+            // Store them back in the network.
+            network.AddSwitch(sw0);
+            network.AddSwitch(sw1);
+        	}
+        }
+    	}
 
-			return network;
-		}
+    	return network;
+    }
 	}
 }
