ed889637c107de09de29ed490f558be3379fed43
Yannick Le Pennec <yannick.lepennec@gmail.com>
2013-12-08 12:22:11 +0900

    Blub

--- flu/controller/controller.ts
+++ flu/controller/controller.ts
@@ -20,10 +20,12 @@ function log(message: string): void {
 class Rule {
     flow_id: number;
     next_hop: string;
+    ttl: number;
 
-    constructor(flowId: number, nextHop: string) {
+    constructor(flowId: number, nextHop: string, ttl: number) {
         this.flow_id = flowId;
         this.next_hop = nextHop;
+        this.ttl = ttl;
     }
 }
 
@@ -39,9 +41,13 @@ class RuleRequest {
  */
 class Controller {
     private static PORT = 8888;
+
+    private rules: Rule[] = [];
     private server: net.Server = null;
 
-    constructor() {
+    constructor(rules: Rule[] = []) {
+        this.rules = rules;
+
         this.server = net.createServer(socket => {
             var clientId = socket.remoteAddress + ':' + socket.remotePort;
 
@@ -57,12 +63,16 @@ class Controller {
             // Data event
             socket.on('data', (data: string) => {
                 try {
-                    this.handleRequest(data);
+                    this.handleRequest(socket, data);
                 } catch (e) {
                     console.error(e);        
                 }
             });
         });
+    }
+
+    public addRule(rule: Rule) {
+        this.rules[rule.flow_id] = rule;
     }
 
     public listen(): void {
@@ -71,11 +81,34 @@ class Controller {
         });
     }
 
-    private handleRequest(data: string): void {
+    private handleRequest(socket: net.NodeSocket, data: string): void {
         var request: RuleRequest = JSON.parse(data);
+        var rule = this.rules[request.flow_id] || null;
 
+        if (rule != null) {
+            log('Providing rule for flow ID ' + request.flow_id);
+            socket.end(JSON.stringify(rule));
+        } else {
+            throw 'No rule found for flow ID ' + request.flow_id;
+        }
     }
 }
 
+// Create the controller
 var controller = new Controller();
+
+[
+    [ 1, '192.168.0.1', 11 ],
+    [ 2, '192.168.0.2', 22 ],
+    [ 3, '192.168.0.3', 33 ],
+    [ 4, '192.168.0.4', 44 ],
+    [ 5, '192.168.0.5', 55 ],
+    [ 6, '192.168.0.6', 66 ],
+    [ 7, '192.168.0.7', 77 ],
+    [ 8, '192.168.0.8', 88 ],
+    [ 9, '192.168.0.9', 99 ],
+].forEach((rule: any[]) => {
+    controller.addRule(new Rule(rule[0], rule[1], rule[2]));
+});
+
 controller.listen();
